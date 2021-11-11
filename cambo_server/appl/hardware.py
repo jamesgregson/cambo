780b24b0f51bde5be215c82089d167837a511e87
james <james.gregson@gmail.com>
2021-11-11 15:48:04 -0800

    added rpi hardware start

--- cambo_server/appl/hardware.py
+++ cambo_server/appl/hardware.py
@@ -25,3 +25,45 @@ class DummyHardware:
     @property
     def quaternion( self ):
         return (1.0, 0.0, 0.0, 0.0)
+
+try:
+    import RPi.GPIO as GPIO
+
+    class RasPiHardware:
+        def __init__( self, *args, **kwargs ):
+            self._name = 'RasPi Hardware'
+            GPIO.setwarnings(False)
+            GPIO.setmod( GPIO.BCM )
+            GPIO.setup( 12, 1000 )
+            GPIO.setup( 13, 1000 )
+            self._pwmL = GPIO.PWM( 12, 1000 )
+            self._pwmR = GPIO.PWM( 13, 1000 )
+            self._dutyL = 0
+            self._dutyR = 0
+            self._pwmL.start(self._dutyL)
+            self._pwmR.start(self._dutyR)
+
+        @property
+        def left_pwm( self ):
+            return self._dutyL
+
+        @left_pwm.setter
+        def left_pwm( self, val ):
+            self._dutyL = max( 0, min( 255, val ) )
+            self._pwmL.ChangeDutyCycle(self._dutyL)
+
+        @property
+        def right_pwm( self ):
+            return self._dutyR
+
+        @right_pwm.setter
+        def right_pwm( self, val ):
+            self._dutyR = max( 0, min( 255, val ) )
+            self._pwmR.ChangeDutyCycle(self._dutyR)
+
+        @property
+        def quaternion( self ):
+            return (1.0, 0.0, 0.0, 0.0)
+
+except:
+    class RasPiHardware(DummyHardware): pass
